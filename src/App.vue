bd0fc5f6bbfbcf14e40a63858cadc471b40a2f0c
PaoloNapoli97 <paolonapoli@live.it>
2022-11-23 16:36:56 +0100

    Aggiunto Esercizio e il Bonus

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,32 @@
 <script>
-import PageTitle from './components/PageTitle.vue';
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
 export default {
   components: {
-    PageTitle
+    AppTitle,
+    AppHero
   }
 }
 </script>
 
 <template>
-  <div>
-    <PageTitle/>
+  <div class="d-flex">
+
+    <div>
+      <AppTitle/>
+    </div>
+
+    <div>
+      <AppHero/>
+    </div>
+    
   </div>
 </template>
 
 <style scoped>
+  d-flex{
+    display: flex;
+    justify-content: center;
+  }
 </style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,31 @@
+<script>
+export default {
+  data() {
+    return {
+      Jumbotron: 'Sono una Jumbotron pezzotta'
+    }
+  }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>{{ Jumbotron }}</h1>
+    </div>
+</template>
+
+<style scoped>
+    .container{
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: auto;
+        width: 90%;
+        background-color: red;
+        height: 600px;
+    }
+
+    h1{
+        font-size: 80px;
+    }
+</style>
